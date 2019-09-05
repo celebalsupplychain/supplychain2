ecc9da151c159a0b871aa833f2db85640bc630b9
ayushaggarwal775 <32215461+ayushaggarwal775@users.noreply.github.com>
2019-09-05 16:08:59 +0530

    Update urls.py

--- SupplyChain/urls.py
+++ SupplyChain/urls.py
@@ -3,10 +3,8 @@ from . import views
 from django.views.generic.base import RedirectView
 
 
-favicon_view = RedirectView.as_view(url='/', permanent=True)
 
 urlpatterns = [path('get_data', views.SupplyChain.as_view()),
                path('uniqueid', views.random_id),
                path('storage_verify', views.azure_functions.as_view()),
-               path('', views.index.as_view()),
-               path('favicon.ico', favicon_view)]
+               path('', views.index.as_view()))]
